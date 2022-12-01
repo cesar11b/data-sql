4bf36b90d77b40caeb4bdc73f137b6c4de5fec7f
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2022-12-01 11:04:59 -0600

    Add files via upload

--- SQLQuery1CesarVivas.sql
+++ SQLQuery1CesarVivas.sql
@@ -1,114 +1,156 @@
-create database CLINICA
---Sistematico 2m2 is 
-use CLINICA
+create database CINEMARK
 
-CREATE TABLE Medicos(
-CodMedico int primary key not null,
-DireccionM nvarchar(50) not null,
-PAMedico nvarchar(40) not null,
-SaMedico int not null,
-SNMedico nvarchar(50) not null,
-PNMedico nvarchar(50) not null,
+USE CINEMARK
 
-Especialidades nvarchar(30) FOREIGN KEY REFERENCES Especialidades(NombreEsp),
-Provincia nvarchar(30) foreign key references Provincia(NombreProv)
+create table Tickets(
+Id_Ticket int primary key identity(1,1) not null,
+Salas int not null,
+Peliculas int not null,
+Precio int not null
+
+Constraint PK_Tickets_Salas
+foreign key (Salas) references Salas(Id_Sala),
+       
+constraint PK_Tickets_Peliculas
+foreign key (Peliculas) references Peliculas(Id_Pelicula)
+      
+
+
+)
+
+Create table VentasT(
+Id_Venta int identity (1,1) primary key not null,
+Fecha_Venta datetime default getdate() not null,
+TotalV float not null
+)
+
+create table TotalV(
+Vendidos int
+
+)
+
+
+
+
+
+
+
+
+
+
+create table Salas(
+Id_Sala int primary key identity(1,1) not null,
+Capacidad_Sala int not null,
+Peliculas int,
+constraint PK_Salas_Peliculas
+foreign key (Peliculas) references Peliculas(Id_Pelicula)
+       on update cascade
+	   on delete cascade,
+
 
 
 
 
 )
-create table Especialidades(
-NombreEsp nvarchar(30) primary key not null,
-DescEsp nvarchar(50) not null
-	)
-
-create table Provincia(
-NombreProv nvarchar(30) primary key not null,
-CantidadPoblad int not null,
-
-Poblacion int foreign key references Poblacion(NombrePobl)
 
 
-
-
-
-)
-create table Poblacion(
-NombrePobl nvarchar(30) primary key not null,
-CantPobl int not null
-
-
-
-
-)
-create table Pacientes(
-CodPac int primary key not null,
-CodPostPac int not null,
-DirPac nvarchar(50) not null,
-SAPac nvarchar(30) not null,
-FNacPac datetime not null,
-SNPac nvarchar(30) not null,
-PaPac nvarchar(30) not null,
-PNPac nvarchar(30) not null,
-
-Provincia Nvarchar(30) foreign key references Provincia(NombreProv)
-
-PacIngres int foreign key references PacIngres(InfoPac)
-
-
-)
-create table Ingresos(
-
-CodIng int primary key not null,
-FechaIng datetime not null,
-Motivo nvarchar (30) not null,
-NHab int not null,
-NCama int not null,
-
-
-Area nvarchar(30) foreign key references AreasClinica(NombreA)
+create table Peliculas(
+Id_Pelicula int primary key identity(1,1) not null,
+Nombre_Pelicula nvarchar(75) not null,
+Genero_Pelicula char(15) not null,
+	
 
 )
 
-create table PacIngres(
-InfoPac nvarchar(50) primary key not null,
-InfoMedico nvarchar(50) not null,
-InfoArea nvarchar(50) not null
+create table Menu_Ind(
+Id_Producto int identity (1,1) primary key not null,
+Nombre_Prod char(30),
+Descripcion varchar(100),
+Precio_Prod int,
+Id_Tienda int,
 
-
-
-)
-create table Egresos(
-
-
-MedicoTratante int foreign key references Medicos(CodMedico),
-FechaEg datetime not null,
-AreaSal int foreign key references AreasClinica(NombreA)
-)
-
-create table AreasClinica(
-NombreA nvarchar(30) primary key not null,
-DescArea nvarchar(50) not null
-
-Habitaciones int foreign key references Habitaciones(NHab)
-
-
-)
-create table Habitaciones(
-Nhab int primary key not null,
-TipoHab nvarchar(30) not null,
-
-Cama int foreign key references Camas(NCama)
-
-
-
-
-)
-create table Camas(
-NCama int primary key not null,
-DescCama nvarchar(30) not null
-
+constraint PK_Menu_Ind_Tiendas
+foreign key (Id_Tienda) references Tiendas(Id_Tienda)
+       on update cascade
+	   on delete cascade,
+Tipo int default 0
 )
 
 
+create table Tiendas(
+Id_Tienda int identity (1,1) primary key not null,
+
+
+)
+
+create table Menu_Combo(
+Id_Producto int identity (1,1) primary key not null,
+Nombre_Prod char(30),
+Descripcion varchar(100),
+Precio_Prod int,
+Id_Tienda int,
+
+constraint PK_Menu_Combo_Tiendas
+foreign key (Id_Tienda) references Tiendas(Id_Tienda)
+       on update cascade
+	   on delete cascade,
+tipo int default 1
+)
+
+
+
+Create table Ventas(
+Id_Venta int identity (1,1) primary key not null,
+Fecha_Venta datetime default getdate() not null,
+Id_Tienda int foreign key references Tiendas(Id_Tienda) not null,
+Tipo bit,
+TotalV float not null
+)
+
+CREATE PROCEDURE NVenta
+@IDTI INT,
+@T bit,
+@C float
+as
+
+if(@T<2)
+begin
+   
+   INSERT INTO Ventas Values(getdate(),@T,@C)
+
+end
+else
+begin
+     print 'se dbe especificar el tipo de producto (0=Individual, 1=Combo)'
+	 END
+
+NVenta 0,1
+
+
+
+create procedure NVentaT
+@CT int
+as
+declare @v as int
+set @v=(Vendidos from TotalV)
+if(@v<=200-@CT)
+begin
+ 
+     insert into VentasT values(getdate(),@CT)
+   
+end
+else
+begin
+  print 'No hay asientos disponibles'
+  end
+NVentaT '3'
+
+
+create trigger ActV
+on VentasT
+after insert
+as
+    DECLARE @CV int=(select TotalV from inserted)
+    update TotalV set Vendidos=Vendidos+@CV
+  
 
