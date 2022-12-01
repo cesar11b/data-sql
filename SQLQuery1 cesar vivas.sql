a1327f2705bf1e74c1d111b6867ac273744c5796
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2022-11-30 22:45:46 -0600

    Update SQLQuery1 cesar vivas.sql

--- SQLQuery1 cesar vivas.sql
+++ SQLQuery1 cesar vivas.sql
@@ -25,3 +25,52 @@ Monto int not null
 
 
 )
+
+
+
+
+
+
+
+create database PracticaExamen
+
+use PracticaExamen
+
+create table Libros(
+Id_Libro int primary key identity(1,1) not null,
+Nombre_Libro varchar(70) not null,
+Cant_Paginas int not null,
+Autor varchar(25) not null,
+Editorial varchar(25) null
+
+)
+
+create table Miembros(
+Id_Miembros int primary key identity(1,1) not null,
+PN varchar(25) not null,
+SN varchar(25),
+PA varchar(25) not null,
+SA varchar(25),
+Estado_M bit default 1
+
+
+)
+
+Create table Prestamos(
+Id_Prestamo int primary key identity(1,1) not null,
+Id_Miembros int not null,
+Id_Libro int not null,
+
+
+constraint PK_Prestamos_Miembros
+foreign key (Id_Miembros) references Miembros(Id_Miembros)
+     ON UPDATE CASCADE
+	 ON DELETE CASCADE,
+constraint PK_Prestamos_Libros
+foreign key (Id_Libro) references Libros
+     ON UPDATE CASCADE
+	 ON DELETE CASCADE,
+
+
+
+)
